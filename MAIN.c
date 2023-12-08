fc16396a83e857cb45d9135d599b28c2af09f1b6
betomukii <mukiibeatrice4@gmail.com>
2023-12-08 08:30:37 +0300

    restored some fixes

---
+++ MAIN.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+#include <ctype.h>
+
+/**
+ * main - the beginning of the program
+ * @void: the main function does not take parameters
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char delimeter[] = " \t\n";
+	char *command = NULL;
+	size_t n = 0;
+
+	while (1)
+	{
+		if (getline(&comand, &n, stdin) == -1)
+			break;
+		if (comand[strlen(comand) - 1] == '\n')
+			comand[strlen(comand) - 1] = '\0';
+		tokenizer(comand, delimeter);
+	}
+	free(comand);
+	return (0);
+}
