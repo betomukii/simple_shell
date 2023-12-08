d38b7c1e58baae7350f1460fe9b8ee4bd10e664f
betomukii <mukiibeatrice4@gmail.com>
2023-12-08 08:03:23 +0300

    HOT FIX

---
+++ aliases.c
@@ -0,0 +1,92 @@
+
+#include "shell.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_aliases - function that prints existing aliases
+ * Return: nothing
+ */
+void print_aliases(void)
+{
+	int alias_count = 0;
+	int i;
+	struct alias_t aliases[MAX_ALIAS_COUNT];
+
+	for (i = 0; i < alias_count; i++)
+	{
+		printf("%s = '%s'\n", aliases[i].name, aliases[i].value);
+	}
+}
+
+/**
+ * set_alias - function that create or modiffy an alias
+ * @name: name of the alias
+ * @value: the string which is the value
+ * Return: nothing
+ */
+void set_alias(char *name, char *value)
+{
+	int alias_count = 0;
+	int i;
+	struct alias_t aliases[MAX_ALIAS_COUNT];
+
+	if (alias_count >= MAX_ALIAS_COUNT)
+	{
+		printf("Error: Too many aliases\n");
+		return;
+	}
+	for (i = 0; i < alias_count; i++)
+	{
+		if (strcmp(aliases[i].name, name) == 0)
+		{
+			_strcpy(aliases[i].value, value);
+			return;
+		}
+	}
+	_strcpy(aliases[alias_count].name, name);
+	_strcpy(aliases[alias_count].value, value);
+	alias_count++;
+}
+
+/**
+ * handle_alias - handles the alias command
+ * @input: the string the user inputs
+ * Return: nothing
+ */
+void handle_alias(char *input)
+{
+	char *token;
+	int i;
+	int alias_count = 0;
+	char name[MAX_ALIAS_NAME_LENGTH];
+	char value[MAX_ALIAS_VALUE_LENGTH];
+	struct alias_t aliases[MAX_ALIAS_COUNT];
+
+	if (_strlen(input) == 5)
+	{
+		print_aliases();
+		return;
+	}
+	token = strtok(input + 6, "=");
+	_strcpy(name, token);
+
+	token = strtok(NULL, "=");
+
+	if (token == NULL)
+	{
+		for (i = 0; i < alias_count; i++)
+		{
+			if (strcmp(aliases[i].name, name) == 0)
+			{
+				printf("%s = '%s'\n", aliases[i].name, aliased[i].value);
+			}
+		}
+	}
+	else
+	{
+		_strcpy(value, token);
+		set_alias(name, value);
+	}
+}
